8af45a2dc94447b7d62f21deb7cc6c9eb843ae1f
Rafi570 <hasan.cse570@gmail.com>
2025-12-06 22:41:53 +0600

    1st commit

---
+++ index.js
@@ -0,0 +1,140 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const admin = require("firebase-admin");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const verifyToken = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+
+  try {
+    const idToken = token.split(" ")[1];
+    const decoded = await admin.auth().verifyIdToken(idToken);
+    console.log("decoded in the token", decoded);
+    req.decoded_email = decoded.email;
+    next();
+  } catch (err) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+};
+
+const uri = process.env.URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// Connect to MongoDB
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB!");
+
+    const db = client.db("Scholarship-Management-System");
+    const userCollection = db.collection("user");
+    const universityCollection = db.collection("university");
+
+    // user related Api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      user.role = "student";
+      user.createdAt = new Date();
+      const email = user.email;
+      const userExists = await userCollection.findOne({ email });
+
+      if (userExists) {
+        return res.send({ message: "user exists" });
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // university related Api
+
+
+    app.get("/scholarships/cheapest",verifyToken, async (req, res) => {
+      const result = await universityCollection
+        .find({})
+        .sort({ scholarshipPostDate: 1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/scholarshipUniversity", async (req, res) => {
+      let query = {};
+
+      const {
+        email,
+        universityName,
+        universityCountry,
+        universityWorldRank,
+        subjectCategory,
+        scholarshipCategory,
+        degree,
+        search,
+      } = req.query;
+
+      if (email) query.postedUserEmail = email;
+      if (universityName) query.universityName = universityName;
+      if (universityCountry) query.universityCountry = universityCountry;
+      if (subjectCategory) query.subjectCategory = subjectCategory;
+      if (scholarshipCategory) query.scholarshipCategory = scholarshipCategory;
+      if (degree) query.degree = degree;
+
+      if (universityWorldRank) {
+        query.universityWorldRank = { $lte: Number(universityWorldRank) };
+      }
+
+      if (search) {
+        query.$or = [
+          { scholarshipName: { $regex: search, $options: "i" } },
+          { universityName: { $regex: search, $options: "i" } },
+        ];
+      }
+
+      const result = await universityCollection
+        .find(query)
+        .sort({ scholarshipPostDate: -1 })
+        .toArray();
+
+      res.send(result); // এখন আর এরর আসবে না
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run();
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
